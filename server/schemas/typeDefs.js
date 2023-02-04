8adffaed4042e15f2af6a51af80096fa250388e5
JKies <jennyhawes24@gmail.com>
2023-02-03 20:49:16 -0600

    adds typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,54 @@
-const typeDefs = ``
+const typeDefs = `
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        bookCount: Int 
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String!
+        authors: [Author]
+        description: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Author {
+        name: String
+    }
+
+    type Auth {
+        token: String
+        user: User
+    }
+
+    input BookInput {
+        bookId: String!
+        authors: [Author]
+        description: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Query {
+        users: [User]
+        me(_id: ID, username: String): User
+    }
+
+    type Mutation {
+        login(username: String, email: String, password: String!): Auth
+
+        addUser(username: String!, email: String!, password: String!): Auth
+
+        saveBook(input: BookInput): User
+
+        removeBook(bookId: String!): User
+    }
+`
 
 module.exports = typeDefs
